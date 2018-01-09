3e8176497e98e7836de061fd657334e35c30c06b
Paul Houghton <paulirotta@gmail.com>
2018-01-09 12:32:34 +0200

    Test command line build

--- generate-legos.ps1
+++ generate-legos.ps1
@@ -3,45 +3,56 @@
 # Part of https://github.com/paulirotta/parametric_lego
 
 
-Function FormatElapsedTime($ts) 
-{
+Function FormatElapsedTime($ts) {
     $elapsedTime = ""
 
-    if ( $ts.Minutes -gt 0 )
-    {
+    if ( $ts.Minutes -gt 0 ) {
         $elapsedTime = [string]::Format( "{0:00}m {1:00}.{2:00}s", $ts.Minutes, $ts.Seconds, $ts.Milliseconds / 10 );
     }
-    else
-    {
+    else {
         $elapsedTime = [string]::Format( "{0:00}.{1:00}s", $ts.Seconds, $ts.Milliseconds / 10 );
     }
 
-    if ($ts.Hours -eq 0 -and $ts.Minutes -eq 0 -and $ts.Seconds -eq 0)
-    {
+    if ($ts.Hours -eq 0 -and $ts.Minutes -eq 0 -and $ts.Seconds -eq 0) {
         $elapsedTime = [string]::Format("{0:00}ms", $ts.Milliseconds);
     }
 
-    if ($ts.Milliseconds -eq 0)
-    {
+    if ($ts.Milliseconds -eq 0) {
         $elapsedTime = [string]::Format("{0}ms", $ts.TotalMilliseconds);
     }
 
     return $elapsedTime
 }
 
-Write-Output "Removing old .stl files"
-Remove-Item "*.stl"
-Remove-Item ".\lego-sign\*.stl"
+Function render($name) {
+    Write-Output "Render $name"
+    $start = Get-Date
+    Invoke-Expression "openscad -o $name.stl $name.scad"
+    $elapsed = FormatElapsedTime ((Get-Date) - $start)
+    Write-Output "Render time: $elapsed for $name"
+    Write-Output ""        
+}
 
-Invoke-Expression ".\lego.ps1 4 2 1"
-Invoke-Expression ".\lego.ps1 2 2 1"
-Invoke-Expression ".\lego.ps1 2 2 1"
+Write-Output "Generating LEGOs"
+Write-Output "================"
+Write-Output "Removing old .stl files"
+Get-ChildItem * -Include *.stl -Recurse | Remove-Item
+
 Invoke-Expression ".\lego.ps1 1 1 1"
+Invoke-Expression ".\lego.ps1 2 2 1"
+Invoke-Expression ".\lego.ps1 2 2 2"
+Invoke-Expression ".\lego.ps1 4 2 1"
 Invoke-Expression ".\technic.ps1 4 4 2"
 
-Write-Output "Render lego-sign.stl"
-$start = Get-Date
-openscad -o ".\lego-sign\lego-sign.stl" ".\lego-sign\lego-sign.scad"
-$elapsed=FormatElapsedTime ((Get-Date) - $start)
-Write-Output "Render time: $elapsed"
-Write-Output ""
+render(".\technic-pin")
+render(".\lego-sign\lego-sign")
+render(".\lego-motor\lego-motor")
+render(".\lego-knob-panel\lego-knob-panel")
+render(".\lego-knob-panel\lego-double_sided-knob-panel")
+render(".\lego-socket-panel\lego-socket-panel")
+render(".\lego-gunrail-mount\lego-gunrail-mount")
+render(".\lego-endcap-enclosure\lego-endcap-enclosure")
+render(".\lego-calibration\lego-calibration")
+render(".\lego-vive-tracker-mount\lego-vive-tracker-mount")
+render(".\lego-vive-tracker-mount\lego-vive-tracker-screw")
+render(".\lego-grove-module-enclosure\lego-grove-module-enclosure")
