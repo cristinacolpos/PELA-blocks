4043825d09773c169b324bf4c9cdb0b75174ae11
Paul Houghton <paulirotta@gmail.com>
2018-02-03 15:01:46 +0200

    filtercleanup improved

--- filtercleanup.ps1
+++ filtercleanup.ps1
@@ -1,12 +1,18 @@
 # Unless you are maintaining a git repo of models, you do not need this script
 #
+# Remove all PNG and STL files from git history to save space before a new build
+git filter-branch -f --tree-filter "rm -rf *.png && rm -rf *.stl" --prune-empty -- --all
+
 # Finish removing dead space from the git repo after filtering out media files
 # Run this _after_ filterstl.ps1 and filterpng.ps1
-
 Write-Output "git reflog expire --expire=now --all"
 git reflog expire --expire=now --all
 
-Write-Output "git gc --prune=now"
-git gc --prune=now
+Write-Output "git gc --aggressive --prune=now"
+git gc --aggressive --prune=now
 
+Invoke-Expression ".\make.ps1 -clean -png"
+
+Write-Output "================================"
 Write-Output "Now type: git push --all --force"
+Write-Output "       (if not changes since this process started)"
