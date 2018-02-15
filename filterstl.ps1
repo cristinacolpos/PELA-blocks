10e59e0e1d632b3df69d85f6dcb22e600cf2dfe1
Paul Houghton <paulirotta@gmail.com>
2018-02-15 17:45:51 +0200

    Git clean; rebuild

--- filterstl.ps1
+++
@@ -1,4 +0,0 @@
-# Unless you are maintaining a git repo of models, you do not need this script
-#
-# Remove all STL files from git history to save space before a new build
-git filter-branch -f --tree-filter "rm -rf *.stl" --prune-empty -- --all
